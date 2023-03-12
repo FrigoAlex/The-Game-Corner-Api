86d10dde47a3c2f25fc3bed14c5974a23f07ed3a
FrigoAlex <alejo012903@hotmail.com>
2023-03-12 00:53:28 -0500

    🔨 Update the navbar component

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,9 +3,22 @@ import "../assets/styles/navbar.css";
 const Navbar = ({ navigateTo }) => {
   return (
     <header className="navbar">
-      <nav className="logo-container" onClick={() => navigateTo("/")}>
+      <div className="logo-container" onClick={() => navigateTo("/")}>
         <img className="header-logo" src="/Logo-Gamer.png" />
         <h1 className="header-title">Gamer Corner</h1>
+      </div>
+      <nav className="nav-section">
+        <ul className="nav-list">
+          <li className="nav-elements" onClick={() => navigateTo("/")}>
+            <a className="nav-elements-a">Home</a>
+          </li>
+          <li className="nav-elements" onClick={() => navigateTo("/login")}>
+            <a className="nav-elements-a">Login</a>
+          </li>
+          <li className="nav-elements" onClick={() => navigateTo("/register")}>
+            <a className="nav-elements-a">Register</a>
+          </li>
+        </ul>
       </nav>
     </header>
   );
