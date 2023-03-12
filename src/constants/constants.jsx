570905afaf4a7c1db8efd1bcbf3a645d72aa2118
FrigoAlex <alejo012903@hotmail.com>
2023-03-12 01:41:59 -0500

    🔧 Add debounceTimer into constants

--- src/constants/constants.jsx
+++ src/constants/constants.jsx
@@ -2,3 +2,6 @@
 export const API_URL = import.meta.env.VITE_API_URL;
 export const API_KEY = import.meta.env.VITE_API_KEY;
 export const USER_API_URL = import.meta.env.VITE_USER_API_URL;
+
+/*  -------------Debounce Timer----------------   */
+export const debounceTimer = 1000;
