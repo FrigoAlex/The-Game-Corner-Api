d66e3fe095e309fde07e4fffcbe54aed1d896a8d
FrigoAlex <alejo012903@hotmail.com>
2023-03-12 00:59:30 -0500

    🔧 Add USER_API_URL into the constants file

--- src/constants/constants.jsx
+++ src/constants/constants.jsx
@@ -1,3 +1,4 @@
 /*  -------------API CONSTANTS-----------------   */
 export const API_URL = import.meta.env.VITE_API_URL;
 export const API_KEY = import.meta.env.VITE_API_KEY;
+export const USER_API_URL = import.meta.env.VITE_USER_API_URL;
