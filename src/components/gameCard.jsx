02d3f58a8efbd6ff4a366276dc49aa82d781cb64
FrigoAlex <alejo012903@hotmail.com>
2023-03-12 15:08:35 -0500

    🔨 Update the gameCard component to the new API

--- src/components/gameCard.jsx
+++ src/components/gameCard.jsx
@@ -1,27 +1,34 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser, faTags, faCalendarDay } from "@fortawesome/free-solid-svg-icons";
+import {
+  faTags,
+  faCalendarDay
+} from "@fortawesome/free-solid-svg-icons";
 
-const gameCard = ({ name, author, description, image, releaseDate, genre, redirect }) => {
+const gameCard = ({
+  name,
+  image,
+  releaseDate,
+  genres,
+  redirect
+}) => {
   return (
     <div className="column">
       <div className="card" onClick={redirect}>
         <img className="card-image" src={image} alt={name} />
         <div className="card-body">
           <h3 className="card-title">{name}</h3>
-          <p className="card-text">{description}</p>
         </div>
         <div className="card-footer">
           <ul className="card-footer-list">
             <li className="card-footer-item">
-              <FontAwesomeIcon className="card-footer-icon" icon={faUser} />
-              {author}
-            </li>
-            <li className="card-footer-item">
               <FontAwesomeIcon className="card-footer-icon" icon={faTags} />
-              {genre}
+              {genres.join(", ")}
             </li>
             <li className="card-footer-item">
-              <FontAwesomeIcon className="card-footer-icon" icon={faCalendarDay} />
+              <FontAwesomeIcon
+                className="card-footer-icon"
+                icon={faCalendarDay}
+              />
               {releaseDate}
             </li>
           </ul>
