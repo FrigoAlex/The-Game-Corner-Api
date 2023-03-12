f8e00e368d83aeabd484afd38f90a93a7deb9034
FrigoAlex <alejo012903@hotmail.com>
2023-03-12 01:06:53 -0500

    ✨ Add getGamesById with the getCommentsBygameId

--- src/utils/dataFetcher.jsx
+++ src/utils/dataFetcher.jsx
@@ -1,9 +1,30 @@
-import { API_URL, API_KEY } from "../constants/constants.jsx";
+import { API_URL, API_KEY, USER_API_URL } from "../constants/constants.jsx";
+
+export const getGameById = async (id) => {
+  try {
+    const response = await fetch(`${API_URL}/${id}?key=${API_KEY}`);
+    const data = await response.json();
+    return {
+      name: data.name,
+      image: data.background_image,
+      rating: data.rating,
+      author: data.developers.map((developer) => developer.name)[0],
+      description: data.description_raw,
+      platforms: data.parent_platforms.map(
+        (platform) => platform.platform.name
+      ),
+      genres: data.genres.map((genre) => genre.name).slice(0, 2),
+      releaseDate: data.released
+    };
+  } catch (error) {
+    return {};
+  }
+};
 
 const dataFetcher = async (actualPage = 1, pageSize = 10, search = "") => {
   try {
     const response = await fetch(
-      `${API_URL}page=${actualPage}&page_size=${pageSize}&search=${search}&key=${API_KEY}`
+      `${API_URL}?page=${actualPage}&page_size=${pageSize}&search=${search}&key=${API_KEY}`
     );
     const data = await response.json();
     return {
@@ -23,4 +44,18 @@ const dataFetcher = async (actualPage = 1, pageSize = 10, search = "") => {
   }
 };
 
+export const getCommentsBygameId = async (id) => {
+  const response = await fetch(`${USER_API_URL}/comments?gameId=${id}`);
+  return await response.json();
+};
+
+export const newComment = async (comment) => {
+  await fetch(`${USER_API_URL}/comments`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(comment)
+  });
+};
 export default dataFetcher;
