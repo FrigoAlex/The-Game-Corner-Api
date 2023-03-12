3020db2fad4a75daf592af1778afad5215fafca3
FrigoAlex <alejo012903@hotmail.com>
2023-03-12 16:35:25 -0500

    ♻️ Refactor the Routing into the app file

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,22 @@ import GamesSection from "./pages/gamesSection";
 import GameDescription from "./pages/gameDescription";
 import Footer from "./components/footer";
 import NoPage from "./components/noPage";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import { HOME, GAME, LOGIN, REGISTER } from "./constants/pageRoutes";
+
 function App () {
   const { path, navigateTo } = useNavigate();
   return (
     <div className="App">
       <Navbar navigateTo={navigateTo} />
-      {path === "/" ? <GamesSection navigateTo={navigateTo} /> : null}
-      {path.match(/\/game\/\d+/) ? <GameDescription /> : null}
-      {path !== "/" && !path.match(/\/game\/\d+/) ? <NoPage /> : null}
+      {
+        [{ url: HOME, page: <GamesSection key={0} navigateTo={navigateTo} /> },
+          { url: GAME, page: <GameDescription key={1} /> },
+          { url: LOGIN, page: <Login key={2} /> },
+          { url: REGISTER, page: <Register key={3} /> }
+        ].find((x) => path.match(new RegExp(x.url)))?.page || <NoPage/>
+      }
       <Footer />
     </div>
   );
