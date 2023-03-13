5f41b4825a3564acb71698ee681f16b56d814e6c
FrigoAlex <alejo012903@hotmail.com>
2023-03-13 03:08:09 -0500

    ♻️ Update the app routing component

--- src/App.jsx
+++ src/App.jsx
@@ -8,17 +8,18 @@ import NoPage from "./components/noPage";
 import Login from "./components/Login";
 import Register from "./components/Register";
 import { HOME, GAME, LOGIN, REGISTER } from "./constants/pageRoutes";
-
+import useUser from "./hooks/useUser";
 function App () {
   const { path, navigateTo } = useNavigate();
+  const { user, login, logOut, errors } = useUser();
   return (
     <div className="App">
-      <Navbar navigateTo={navigateTo} />
+      <Navbar navigateTo={navigateTo} user={user} logout={logOut} />
       {
         [{ url: HOME, page: <GamesSection key={0} navigateTo={navigateTo} /> },
-          { url: GAME, page: <GameDescription key={1} /> },
-          { url: LOGIN, page: <Login key={2} /> },
-          { url: REGISTER, page: <Register key={3} /> }
+          { url: GAME, page: <GameDescription key={1} user={user}/> },
+          { url: LOGIN, page: <Login login={login} errors={errors} user={user} navigateTo={navigateTo} key={2} /> },
+          { url: REGISTER, page: <Register navigateTo={navigateTo} key={3} /> }
         ].find((x) => path.match(new RegExp(x.url)))?.page || <NoPage/>
       }
       <Footer />
